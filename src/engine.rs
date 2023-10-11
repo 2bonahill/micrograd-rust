b4c28e9c2e99694b6ddd5dd4e3d6e8f202505621
Tobias <tobias.ambuehl@gmail.com>
2023-10-11 10:28:59 +0200

    Final architecture

--- src/engine.rs
+++ src/engine.rs
@@ -1,11 +1,189 @@
 use std::{
     cell::RefCell,
+    f64::consts::E,
     fmt::Display,
-    ops::{Add, Div, Mul, Neg, Sub},
+    ops::{Add, Deref, Div, Mul, Neg, Sub},
     rc::Rc,
 };
 
-#[derive(Debug, PartialEq)]
+#[derive(Clone, Debug)]
+pub struct Value(Rc<RefCell<ValueInner>>);
+
+impl Value {
+    pub fn new(data: f64) -> Self {
+        Value(Rc::new(RefCell::new(ValueInner::new(data))))
+    }
+
+    pub fn new_from_inner(inner: ValueInner) -> Self {
+        Value(Rc::new(RefCell::new(inner)))
+    }
+
+    pub fn data(&self) -> f64 {
+        // Thanks to the Deref implementation we can call borrow on self directly
+        self.borrow().data
+    }
+
+    pub fn grad(&self) -> f64 {
+        self.borrow().grad
+    }
+
+    pub fn op(&self) -> Op {
+        self.borrow().op
+    }
+
+    pub fn set_grad(&self, new_grad: f64) {
+        self.borrow_mut().grad = new_grad;
+    }
+
+    pub fn pow(&self, exponent: f64) -> Self {
+        let out = ValueInner {
+            data: self.borrow().data.powf(exponent),
+            grad: 0.0,
+            prev: vec![self.clone()],
+            op: Op::Add,
+        };
+
+        Value::new_from_inner(out)
+    }
+
+    pub fn tanh(&self) -> Self {
+        let x = self.borrow().data;
+        let epowf2x = E.powf(2.0 * x);
+        let data = (epowf2x - 1.0) / (epowf2x + 1.0);
+        let out = ValueInner {
+            data,
+            grad: 0.0,
+            prev: vec![self.clone()],
+            op: Op::Tanh,
+        };
+
+        Value::new_from_inner(out)
+    }
+
+    pub fn _backward(&self) {
+        let inner = self.borrow();
+        let op = inner.op;
+        let grad = inner.grad;
+        let prev = inner.prev.clone();
+
+        match op {
+            Op::Add => {
+                // The operation was c1 + c2
+                prev[0].set_grad(grad);
+                prev[1].set_grad(grad);
+            }
+            Op::Sub => {
+                // The operation was c1 - c2
+                prev[0].set_grad(grad);
+                prev[1].set_grad(-grad);
+            }
+            Op::Mul => {
+                // The operation was c1 * c2
+                prev[0].set_grad(grad * prev[1].data());
+                prev[1].set_grad(grad * prev[0].data());
+            }
+            Op::Div => todo!(),
+            Op::Neg => todo!(),
+            Op::Pow => todo!(),
+            Op::Tanh => todo!(),
+            Op::None => {}
+        }
+    }
+}
+
+impl Deref for Value {
+    type Target = Rc<RefCell<ValueInner>>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl Add<Value> for Value {
+    type Output = Value;
+
+    fn add(self, other: Self) -> Self::Output {
+        let out = ValueInner {
+            data: self.borrow().data + other.borrow().data,
+            grad: 0.0,
+            prev: vec![self.clone(), other.clone()],
+            op: Op::Add,
+        };
+
+        Value::new_from_inner(out)
+    }
+}
+
+impl Add<f64> for Value {
+    type Output = Value;
+
+    fn add(self, other: f64) -> Self::Output {
+        self.borrow_mut().data += other;
+        self
+    }
+}
+
+impl Sub<Value> for Value {
+    type Output = Value;
+
+    fn sub(self, other: Self) -> Self::Output {
+        let out = ValueInner {
+            data: self.borrow().data - other.borrow().data,
+            grad: 0.0,
+            prev: vec![self.clone(), other.clone()],
+            op: Op::Sub,
+        };
+
+        Value::new_from_inner(out)
+    }
+}
+
+impl Mul for Value {
+    type Output = Value;
+
+    fn mul(self, other: Self) -> Self::Output {
+        let out = ValueInner {
+            data: self.borrow().data * other.borrow().data,
+            grad: 0.0,
+            prev: vec![self.clone(), other.clone()],
+            op: Op::Mul,
+        };
+
+        Value::new_from_inner(out)
+    }
+}
+
+impl Neg for Value {
+    type Output = Value;
+
+    fn neg(self) -> Self::Output {
+        self.borrow_mut().data *= -1.0;
+        self
+    }
+}
+
+impl Div for Value {
+    type Output = Value;
+
+    fn div(self, other: Self) -> Self::Output {
+        let out = ValueInner {
+            data: self.borrow().data / other.borrow().data,
+            grad: 0.0,
+            prev: vec![self.clone(), other.clone()],
+            op: Op::Div,
+        };
+
+        Value::new_from_inner(out)
+    }
+}
+
+impl Display for Value {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Value(data={})", self.borrow().data)
+    }
+}
+
+#[derive(Debug, PartialEq, Clone, Copy)]
 pub enum Op {
     None,
     Add,
@@ -14,119 +192,34 @@ pub enum Op {
     Div,
     Pow,
     Neg,
+    Tanh,
 }
 
-#[derive(Debug)]
-pub struct Value {
+#[derive(Debug, Clone)]
+pub struct ValueInner {
     pub data: f64,
     pub grad: f64,
-    pub _prev: Vec<Rc<RefCell<Value>>>,
-    pub _op: Op,
+    pub prev: Vec<Value>,
+    pub op: Op,
 }
 
-impl Value {
+impl ValueInner {
     pub fn new(data: f64) -> Self {
-        Value {
+        ValueInner {
             data,
             grad: 0.0,
-            _prev: Vec::new(),
-            _op: Op::None,
+            prev: Vec::new(),
+            op: Op::None,
         }
     }
 
     pub fn new_with_op(data: f64, op: Op) -> Self {
-        Value {
+        ValueInner {
             data,
             grad: 0.0,
-            _prev: Vec::new(),
-            _op: op,
+            prev: Vec::new(),
+            op,
         }
-    }
-
-    pub fn _backward(&self) {
-        let mut c1 = self._prev[0].borrow_mut();
-        let mut c2 = self._prev[1].borrow_mut();
-        match self._op {
-            Op::Add | Op::Sub => {
-                // The operation was c1 + c2
-                c1.grad = self.grad;
-                c2.grad = self.grad;
-            }
-            Op::Mul => todo!(),
-            Op::Div => todo!(),
-            Op::Neg => todo!(),
-            Op::Pow => todo!(),
-            Op::None => {}
-        }
-    }
-
-    pub fn pow(&self, exponent: f64) -> Self {
-        let new_data = self.data.powf(exponent);
-        Value::new_with_op(new_data, Op::Pow)
-    }
-}
-
-impl Add for Value {
-    type Output = Value;
-
-    fn add(self, other: Self) -> Self::Output {
-        let mut out = Value {
-            data: self.data + other.data,
-            grad: 0.0,
-            _prev: Vec::new(),
-            _op: Op::Add,
-        };
-
-        out._prev.push(Rc::new(RefCell::new(self)));
-        out._prev.push(Rc::new(RefCell::new(other)));
-
-        out
-    }
-}
-
-impl Add<f64> for Value {
-    type Output = Value;
-
-    fn add(self, other: f64) -> Self::Output {
-        Value::new_with_op(self.data + other, Op::Add)
-    }
-}
-
-impl Sub for Value {
-    type Output = Value;
-
-    fn sub(self, other: Self) -> Self::Output {
-        Value::new_with_op(self.data - other.data, Op::Sub)
-    }
-}
-
-impl Mul for Value {
-    type Output = Value;
-
-    fn mul(self, other: Self) -> Self::Output {
-        Value::new_with_op(self.data * other.data, Op::Mul)
-    }
-}
-
-impl Neg for Value {
-    type Output = Value;
-
-    fn neg(self) -> Self::Output {
-        Value::new_with_op(-self.data, Op::Neg)
-    }
-}
-
-impl Div for Value {
-    type Output = Value;
-
-    fn div(self, other: Self) -> Self::Output {
-        Value::new_with_op(self.data / other.data, Op::Div)
-    }
-}
-
-impl Display for Value {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Value(data={})", self.data)
     }
 }
 
@@ -140,50 +233,65 @@ mod tests {
         let a: Value = Value::new(2.0);
         let b: Value = Value::new(3.0);
         let c: Value = a + b;
-        assert_eq!(c.data, 5.0);
-        assert_eq!(c._op, Op::Add);
+        assert_eq!(c.data(), 5.0);
+        assert_eq!(c.op(), Op::Add);
     }
+    /*
+        #[test]
+        fn test_add_backward() {
+            let a: Value = Value::new(2.0);
+            let b: Value = Value::new(3.0);
+            assert_eq!(a.grad, 0.0);
+            assert_eq!(b.grad, 0.0);
+            let mut c: Value = a + b;
+            dbg!(&c);
+            c.grad = 2.0;
+            assert_eq!(c.prev[0].borrow().grad, 2.0);
+            assert_eq!(c.prev[0].borrow().grad, 2.0);
+        }
 
-    #[test]
-    fn test_sub() {
-        let a: Value = Value::new(2.0);
-        let b: Value = Value::new(3.0);
-        let c: Value = a - b;
-        assert_eq!(c.data, -1.0);
-        assert_eq!(c._op, Op::Sub);
-    }
+        #[test]
+        fn test_sub() {
+            let a: Value = Value::new(2.0);
+            let b: Value = Value::new(3.0);
+            let c: Value = a - b;
+            assert_eq!(c.data, -1.0);
+            assert_eq!(c.op, Op::Sub);
+        }
 
-    #[test]
-    fn test_mul() {
-        let a: Value = Value::new(2.0);
-        let b: Value = Value::new(3.0);
-        let c: Value = a * b;
-        assert_eq!(c.data, 6.0);
-        assert_eq!(c._op, Op::Mul);
-    }
+        #[test]
+        fn test_mul() {
+            let a: Value = Value::new(2.0);
+            let b: Value = Value::new(3.0);
+            let c: Value = a * b;
+            assert_eq!(c.data, 6.0);
+            assert_eq!(c.op, Op::Mul);
+        }
 
-    #[test]
-    fn test_pow() {
-        let a: Value = Value::new(3.0);
-        let b: Value = a.pow(2.0);
-        assert_eq!(b.data, 9.0);
-        assert_eq!(b._op, Op::Pow);
-    }
+        #[test]
+        fn test_pow() {
+            let a: Value = Value::new(3.0);
+            let b: Value = a.pow(2.0);
+            assert_eq!(b.data, 9.0);
+            assert_eq!(b.op, Op::Pow);
+        }
 
-    #[test]
-    fn test_div() {
-        let a: Value = Value::new(3.0);
-        let b: Value = Value::new(2.0);
-        let c: Value = a / b;
-        assert_eq!(c.data, 1.5);
-        assert_eq!(c._op, Op::Div);
-    }
+        #[test]
+        fn test_div() {
+            let a: Value = Value::new(3.0);
+            let b: Value = Value::new(2.0);
+            let c: Value = a / b;
+            assert_eq!(c.data, 1.5);
+            assert_eq!(c.op, Op::Div);
+        }
 
-    #[test]
-    fn test_neg() {
-        let a: Value = Value::new(3.0);
-        let b = -a;
-        assert_eq!(b.data, -3.0);
-        assert_eq!(b._op, Op::Neg);
-    }
+        #[test]
+        fn test_neg() {
+            let a: Value = Value::new(3.0);
+            let b = -a;
+            assert_eq!(b.data, -3.0);
+            assert_eq!(b.op, Op::Neg);
+        }
+
+    */
 }
--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,45 @@ extern crate micrograd_rust;
 
 fn main() {
     println!("Welcome to Micrograd-Rust");
+    simple();
+    // manual_grad_simple();
+}
+
+pub fn simple() {
     let a: Value = Value::new(2.0);
     let b: Value = Value::new(3.0);
-    let mut c: Value = a + b;
-
-    c.grad = 1.0;
-    dbg!(&c);
+    let c: Value = a * b;
+    // dbg!(&c);
+    c.set_grad(1.0);
     c._backward();
+    dbg!("AFter backward");
     dbg!(&c);
 }
 
-pub fn manual_grad() {
+pub fn manual_grad_simple() {
+    // o = a + b
+    let delta: f64 = 0.001;
+
+    // Base
+    let a: Value = Value::new(2.0);
+    let b: Value = Value::new(3.0);
+    let o1: Value = a + b;
+    println!("o1: {}", o1);
+
+    // Notch
+    let a: Value = Value::new(2.0);
+    let a: Value = a + delta;
+    let b: Value = Value::new(3.0);
+    let o2: Value = a + b;
+    println!("o2: {}", o2);
+
+    // Grad
+    let grad = (o2.data() - o1.data()) / delta;
+    println!("Grad of o with respect to a: {}", grad);
+    dbg!(&o2);
+}
+/*
+pub fn manual_grad_complex() {
     // o = a*b + c
     let delta: f64 = 0.001;
 
@@ -39,3 +67,4 @@ pub fn manual_grad() {
     let grad = (o2.data - o1.data) / delta;
     println!("Grad: {}", grad);
 }
+*/
